19b6180751a2f34f41a6f5c77a4062c326353545
Chris Conlon <chris@wolfssl.com>
2017-04-07 14:10:28 -0600

    add custom print to test.c for PB

--- wolfcrypt/test/test.c
+++ wolfcrypt/test/test.c
@@ -175,6 +175,13 @@
         #define printf dc_log_printf
 #endif
 
+#ifdef WOLFSSL_PB
+    #include <stdarg.h>
+    int wolfssl_pb_print(const char*, ...);
+    #undef printf
+    #define printf wolfssl_pb_print
+#endif
+
 #include "wolfcrypt/test/test.h"
 
 #ifdef USE_WOLFSSL_MEMORY
@@ -346,6 +353,23 @@ static void myFipsCb(int ok, int err, const char* hash)
         static byte gTestMemory[100000];
     #endif
 #endif
+
+#ifdef WOLFSSL_PB
+int wolfssl_pb_print(const char* msg, ...)
+{
+    int ret;
+    va_list args;
+    char tmpBuf[80];
+
+    va_start(args, msg);
+    ret = vsprint(tmpBuf, msg, args);
+    va_end(args);
+
+    fnDumpStringToSystemLog(tmpBuf);
+
+    return ret;
+}
+#endif /* WOLFSSL_PB */
 
 #ifdef HAVE_STACK_SIZE
 THREAD_RETURN WOLFSSL_THREAD wolfcrypt_test(void* args)
